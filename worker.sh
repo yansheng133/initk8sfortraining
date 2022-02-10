c9efd83c2af1d23e0ad82ea462cd201541e0f3f6
YanSheng <sam012822@gmail.com>
2022-02-10 13:28:34 +0800

    fix worker.sh for 1.22 and etc

--- worker.sh
+++ worker.sh
@@ -52,10 +52,34 @@ printf "${GRN}--install nfs package-- ${NC}\n"
 sudo apt -y install nfs-common
 sudo sed -i 's/# Domain = localdomain/Domain = training.inwinstack/g' /etc/idmapd.conf
 
-printf "${GRN}--Install Docker-- ${NC}\n"
+printf "${GRN}--Install Docker CE and ContainerD--${NC}\n"
 sleep 1
 
-sudo apt-get install -y docker.io
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sudo sysctl --system
+
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo systemctl restart containerd
+
 printf "${GRN}--install kubeadm, kubelet, and kubectl-- ${NC}\n"
 sleep 1
 
@@ -65,7 +89,7 @@ sudo sh -c "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-
 
 sudo apt-get update
 
-sudo apt-get install -y kubeadm=1.20.1-00 kubelet=1.20.1-00 kubectl=1.20.1-00
+sudo apt-get install -y kubeadm=1.22.1-00 kubelet=1.22.1-00 kubectl=1.22.1-00
 
 printf "${YEL}--LOCK kubelet kubeadm kubectl version-- ${NC}\n"
 sudo apt-mark hold kubelet kubeadm kubectl
